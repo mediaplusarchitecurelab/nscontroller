24db34823c6b7b55ae4ad79054ff5fc274e96d6e
mplusalab <43529049+mplusalab@users.noreply.github.com>
2018-11-01 00:22:04 +0800

    First commit

---
+++ sketch.js
@@ -0,0 +1,155 @@
+
+const buttonNum = 31;
+
+const buttonMapping = {
+    0: 'A',
+    1: 'X',
+    2: 'B',
+    3: 'Y',
+    4: 'RSL',
+    5: 'RSR',
+    9: 'PLUS',
+    11: 'RA',
+    12: 'HOME',
+    14: 'R',
+    15: 'RT',
+    16: 'LEFT',
+    17: 'DOWN',
+    18: 'UP',
+    19: 'RIGHT',
+    20: 'LSL',
+    21: 'LSR',
+    24: 'MINUS',
+    26: 'LA',
+    29: 'CAPTURE',
+    30: 'L',
+    31: 'LT'
+}
+
+const dot = new DOT(400,300);
+function setup() {
+  createCanvas(800,600);
+  textAlign(LEFT,CENTER);
+  ellipseMode(CENTER);
+  //dot=
+}
+
+function draw() {
+  background(200);
+  
+
+  let buttonCount = 0;
+  for(let p = 0; p <= buttonNum; p++) {
+    const name = buttonMapping[p] || p;
+    if(isNaN(name)){
+      fill(0);
+      text(name, 100, 25 * buttonCount + 30);
+      noFill();
+      ellipse(85, 25 * buttonCount + 30, 13, 13);
+      buttonCount++;
+    }
+  }
+
+  let gamepads = navigator.getGamepads ? navigator.getGamepads() : [];
+  let gamepadArray = [];
+
+  // 尋找控制器
+  let orderedGamepads = [];
+  let joyconR,joyconL;
+  for(let i = 0; i < gamepads.length; i++) {
+    gamepadArray.push(gamepads[i]);
+    
+   //避免控制器list為null
+    if (gamepadArray[i] === null){
+    }else{//win10之右控制器id名為  Wireless Gamepad (Vendor: 057e Product: 2007)
+      if (gamepadArray[i].id === 'Wireless Gamepad (Vendor: 057e Product: 2007)'){
+          joyconR = i;
+      }else{
+          joyconL = i;
+      }
+    }
+  }
+  orderedGamepads.push(gamepadArray[joyconR]);
+  orderedGamepads.push(gamepadArray[joyconL]);
+
+  let pressed = [];
+
+    for (let g = 0; g < orderedGamepads.length; g++) {
+        const gp = orderedGamepads[g];
+        if (!!gp) {
+            const axes = gp.axes;
+            let arrowX = 200;
+            arrowX += g == 0 ? 100 : 0;
+
+            // スティックの位置をマッピング
+            fill(0);
+            // 方向入れる
+            if(axes[axes.length-1] <= 1){
+              let axesVal = 0;
+              if(g == 0){
+                axesVal = map(axes[axes.length-1], -1, 1, 0, 7) - 4;
+                
+                //控制移動
+                dot.move( 1 * sin(radians(axesVal * 360 / 8)), 1 * cos(radians(axesVal * 360 / 8)),);
+              }
+              else {
+                axesVal = map(axes[axes.length-1], -1, 1, 0, 7);
+                //console.log(3 * cos(radians(axesVal * 360 / 8)));
+              }
+              let axesTheta = axesVal * 360 / 8;
+
+              //移動
+
+              //dot.move(3 * cos(radians(axesTheta))+ arrowX , 3 * sin(radians(axesTheta))+ 100, 20, 20);
+              //ellipse(30 * cos(radians(axesTheta))+ arrowX , 30 * sin(radians(axesTheta))+ 100, 20, 20);
+            }
+            // ニュートラル位置
+            else {
+              ellipse(arrowX, 100, 20, 20);
+            }
+
+            for(let i = 0; i < gp.buttons.length; i++) {
+
+                if(gp.buttons[i].pressed) {
+                    const pressedId = (g * 15) + i + g;
+                    const pressedIndex = getIndex(pressedId);;
+
+                    fill(200, 0, 0);
+                    ellipse(85, 25 * pressedIndex + 30, 13, 13);
+                }
+            }
+        }
+    }
+    // drawing
+    dot.display();
+}
+
+function getIndex(id) {
+  var resultIndex = 0
+  for(let i = 0; i < id; i++) {
+    const name = buttonMapping[i] || i;
+    if(isNaN(name)){
+      resultIndex++;
+    }
+  }
+
+  return resultIndex;
+}
+
+function DOT(x,y){
+  this.x = x;
+  this.y = y;
+  this.diameter = 15;
+
+  this.move = function(dx,dy){
+    this.x-=dx;
+    this.y+=dy;
+  }
+
+  this.display = function(){
+    
+    noStroke();
+    fill(200, 0, 0);
+    ellipse(this.x, this.y, this.diameter, this.diameter);
+  }
+}
